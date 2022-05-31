29b1e833619e98c9ede65edef589634120ffb825
dmonstermike <dmonstermike@gmail.com>
2022-05-31 20:24:20 +0900

    first init

--- lib/main.dart
+++ lib/main.dart
@@ -23,68 +23,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late final NamesCubit cubit;
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Home Page"),
-        ),
-        body: StreamBuilder<String?>(
-          stream: cubit.stream,
-          builder: (context, snapshot) {
-            final button = TextButton(
-              onPressed: () {
-                cubit.pickRandomName();
-              },
-              child: Text("Pick a random name"),
-            );
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return button;
-              case ConnectionState.waiting:
-                return button;
-              case ConnectionState.active:
-                return Column(
-                  children: [
-                    Text(snapshot.data ?? " "),
-                    button,
-                  ],
-                );
-              case ConnectionState.done:
-                return const SizedBox();
-            }
-          },
-        ));
+    return Container();
   }
-
-  void dispose() {
-    cubit.close();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    cubit = NamesCubit();
-  }
-}
-
-class NamesCubit extends Cubit<String?> {
-  NamesCubit() : super(null);
-
-  void pickRandomName() => emit(names.getRandomElement());
-}
-
-extension RandomElement<T> on Iterable<T> {
-  T getRandomElement() => elementAt(math.Random().nextInt(length));
 }
